7f2e6f75aeb91f9d0dc74e11ea5e562dc886d162
David <david@webcodr.io>
2025-05-11 23:51:15 +0200

    Refactor database configuration

--- src/main/kotlin/io/pcast/plugins/Database.kt
+++ src/main/kotlin/io/pcast/plugins/Database.kt
@@ -2,49 +2,54 @@ package io.pcast.plugins
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import io.ktor.server.application.Application
 import io.pcast.model.feed.FeedsTable
 import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
-import javax.sql.DataSource
 
 private const val DATABASE_JDBC_URL = "jdbc:postgresql://localhost:5433/pcast"
 private const val TEST_DATABASE_JDBC_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
+private const val DATABASE_MIGRATIONS_LOCATION = "db/migration/postgres"
+private const val TEST_DATABASE_MIGRATIONS_LOCATION = "db/migration/h2"
 private const val POSTGRES_DRIVER = "org.postgresql.Driver"
 private const val H2_DRIVER = "org.h2.Driver"
 private const val POSTGRES_USER = "pcast"
 private const val POSTGRES_PASSWORD = "pcast"
 
-fun Application.configureDatabase() = connectAndMigrate(
+fun configureDatabase() = connectAndMigrate(
     dataSource = hikariDataSource {
         jdbcUrl = DATABASE_JDBC_URL
         driverClassName = POSTGRES_DRIVER
         username = POSTGRES_USER
         password = POSTGRES_PASSWORD
     },
-    migrationLocation = "db/migration/postgres"
+    migrationLocation = DATABASE_MIGRATIONS_LOCATION
 )
 
-
-fun Application.configureTestDatabase() = connectAndMigrate(
+fun configureTestDatabase() = connectAndMigrate(
     dataSource = hikariDataSource {
         jdbcUrl = TEST_DATABASE_JDBC_URL
         driverClassName = H2_DRIVER
     },
-    migrationLocation = "db/migration/h2"
+    migrationLocation = TEST_DATABASE_MIGRATIONS_LOCATION
 )
 
 private fun connectAndMigrate(
     dataSource: HikariDataSource,
     migrationLocation: String
 ): Database {
-    migrateDatabase(dataSource, migrationLocation)
+    flyway {
+        dataSource(dataSource)
+        locations(migrationLocation)
+    }
 
     val db = Database.connect(dataSource)
 
-    createSchemas(db)
+    transaction(db) {
+        SchemaUtils.create(FeedsTable)
+    }
 
     Runtime.getRuntime().addShutdownHook(Thread {
         db.connector().close()
@@ -58,20 +63,6 @@ private fun hikariDataSource(
     lambda: HikariConfig.() -> Unit
 ) = HikariDataSource(HikariConfig().apply(lambda))
 
-private fun migrateDatabase(
-    dataSource: DataSource,
-    migrationLocation: String
-) {
-    Flyway
-        .configure()
-        .dataSource(dataSource)
-        .locations(migrationLocation)
-        .load()
-        .migrate()
-}
-
-private fun createSchemas(db: Database) {
-    transaction(db) {
-        SchemaUtils.create(FeedsTable)
-    }
-}
+private fun flyway(
+    lambda: FluentConfiguration.() -> Unit
+) = Flyway.configure().apply(lambda).load().migrate()
