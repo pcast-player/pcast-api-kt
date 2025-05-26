f42bf23b0af9d8ec86ef11ec0a18da2319a461d6
David <david@webcodr.io>
2025-05-26 22:05:56 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
 val kotlinVersion: String by project
 val logbackVersion: String by project
 val ktorVersion: String by project
+val exposedVersion: String by project
+val flywayVersion: String by project
+val hikariVersion: String by project
+val postgresVersion: String by project
+val h2Version: String by project
+val hopliteVersion: String by project
+val xmlUtilVersion: String by project
 
 plugins {
-    kotlin("jvm") version "2.1.20"
-    id("io.ktor.plugin") version "3.1.2"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
+    kotlin("jvm") version "2.1.21"
+    id("io.ktor.plugin") version "3.1.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21"
 }
 
 group = "io.pcast"
@@ -34,27 +41,29 @@ dependencies {
     implementation("com.fasterxml.uuid:java-uuid-generator:5.1.0")
 
     // Exposed + database drivers
-    implementation("org.jetbrains.exposed:exposed-core:0.48.0")
-    implementation("org.jetbrains.exposed:exposed-dao:0.48.0")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.48.0")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.48.0")
-    implementation("org.flywaydb:flyway-core:11.8.0")
-    runtimeOnly("org.flywaydb:flyway-database-postgresql:11.8.0")
-    implementation("com.zaxxer:HikariCP:6.3.0")
-    implementation("org.postgresql:postgresql:42.7.5")
-    implementation("com.h2database:h2:2.2.224")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    implementation("com.zaxxer:HikariCP:$hikariVersion")
+    implementation("org.postgresql:postgresql:$postgresVersion")
+    implementation("com.h2database:h2:$h2Version")
+
+    // Flyway
+    implementation("org.flywaydb:flyway-core:${flywayVersion}")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:${flywayVersion}")
 
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
     testImplementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-server-call-logging")
 
-    implementation("com.sksamuel.hoplite:hoplite-core:2.9.0")
-    implementation("com.sksamuel.hoplite:hoplite-hocon:2.9.0")
+    implementation("com.sksamuel.hoplite:hoplite-core:$hopliteVersion")
+    implementation("com.sksamuel.hoplite:hoplite-hocon:$hopliteVersion")
 
     implementation("cash.z.ecc.android:kotlin-bip39:1.0.9")
     implementation("io.viascom.nanoid:nanoid:1.0.1")
 
-    implementation("io.github.pdvrieze.xmlutil:core-jdk:0.91.1")
-    implementation("io.github.pdvrieze.xmlutil:serialization-jvm:0.91.1")
+    implementation("io.github.pdvrieze.xmlutil:core-jdk:$xmlUtilVersion")
+    implementation("io.github.pdvrieze.xmlutil:serialization-jvm:$xmlUtilVersion")
 }
