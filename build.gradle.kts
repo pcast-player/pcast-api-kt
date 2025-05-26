d377fb5a501b768922f394affac0b3cd9d836bc9
David <david@webcodr.io>
2025-05-26 21:19:45 +0200

    Refactor to camel case

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-val kotlin_version: String by project
-val logback_version: String by project
-val ktor_version: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val ktorVersion: String by project
 
 plugins {
     kotlin("jvm") version "2.1.20"
@@ -30,7 +30,7 @@ dependencies {
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.6.2")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("com.fasterxml.uuid:java-uuid-generator:5.1.0")
 
     // Exposed + database drivers
@@ -45,8 +45,8 @@ dependencies {
     implementation("com.h2database:h2:2.2.224")
 
     testImplementation("io.ktor:ktor-server-test-host-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    testImplementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-server-call-logging")
 
     implementation("com.sksamuel.hoplite:hoplite-core:2.9.0")
